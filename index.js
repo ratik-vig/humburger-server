e175b611a06b4cb9197c8cd8403e5ef7b7269c11
Ratik Vig <ratikvig@gmail.com>
2023-05-30 17:33:29 -0400

    added db connection

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
 
+const db = require('./utils/db')
+
 const app = express()
 app.use(cors())
 app.use(express.json())
---
+++ utils/db.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, 
+  idleTimeout: 60000,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
+});
+
+module.exports = pool
